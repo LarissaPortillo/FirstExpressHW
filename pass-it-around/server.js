5daddae0b8e0eb18b8afb2ae5c5866ef3d4dc6ee
Larissa Portillo <larissaportillo10@gmail.com>
2022-07-27 19:41:36 -0400

    minor edits

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -17,7 +17,7 @@ app.get('/bug',(req,res)=>{
     let rnd=Math.floor(Math.random()*(128-99)+99)
     let arrBug=[98,98,98,rnd];
     let bugs=arrBug[Math.floor(Math.random()*arrBug.length)]
-    res.send(`<h1> 99 little bugs in the code </h1> <h2>99 little bugs<h2>  <a href= http://localhost:3000/bug/${bugs}>Take one down, patch it around. ${bugs} bugs in the code</a> `)
+    res.send(`<h1> 99 little bugs in the code </h1> <h2>99 little bugs<h2>  <a href= http://localhost:3000/bug/${bugs}>Take one down, patch it around.</a> <h2>${bugs} bugs in the code</h2> `)
 });
 
 app.get('/bug/:number_of_bugs',(req,res)=>{
@@ -29,7 +29,7 @@ app.get('/bug/:number_of_bugs',(req,res)=>{
         res.send(`<h1>No more little bugs in the code </h1> <h2>No more little bugs.</h2>  <a href=http://localhost:3000/bug >Start Over</a>`);
     }
     else{
-        res.send(`<h1>${req.params.number_of_bugs} little bugs in the code </h1> <h2>${req.params.number_of_bugs} little bugs<h2>  <a href= http://localhost:3000/bug/${bugs}>Take one down, pass it around. ${bugs} bugs in the code. </a>  `)
+        res.send(`<h1>${req.params.number_of_bugs} little bugs in the code </h1> <h2>${req.params.number_of_bugs} little bugs<h2>  <a href= http://localhost:3000/bug/${bugs}>Take one down, pass it around. </a> <h2> ${bugs} bugs in the code.<h2>  `)
     }
 });
 
