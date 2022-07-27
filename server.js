a81f5ff5a272afc4d6c95909f0d5ec4a99c95808
Larissa Portillo <larissaportillo10@gmail.com>
2022-07-27 12:06:41 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+
+require('dotenv').config();
+const app=express();
+const port=process.env.PORT || 3003;
+
+//routes
+//
+// app.get('/greeting',(req,res)=>{
+//     res.send('Hello, stranger')
+// })
+
+// app.get('/greeting/:name',(req,res)=>{
+//     res.send("What's up, "+req.params.name +'!');
+// })
+
+app.listen(port,()=>{
+    console.log('I am listening on ',port);
+})
