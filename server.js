256d9fa5a61eb8f4a79852186e1cd8ad5635637c
Larissa Portillo <larissaportillo10@gmail.com>
2022-07-27 13:06:05 -0400

    Greeting express application created.

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app=express();
 const port=process.env.PORT || 3003;
 
 //routes
-//
-// app.get('/greeting',(req,res)=>{
-//     res.send('Hello, stranger')
-// })
+//Greeting express application created.
+app.get('/greeting',(req,res)=>{
+    res.send('Hello, stranger')
+})
 
-// app.get('/greeting/:name',(req,res)=>{
-//     res.send("What's up, "+req.params.name +'!');
-// })
+app.get('/greeting/:name',(req,res)=>{
+    res.send("What's up, "+req.params.name +'!');
+})
 
 app.listen(port,()=>{
     console.log('I am listening on ',port);
