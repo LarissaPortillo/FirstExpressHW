661b69263249920104432117ef03665de506a3c2
Larissa Portillo <larissaportillo10@gmail.com>
2022-07-27 14:12:36 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -6,18 +6,30 @@ const port=process.env.PORT || 3003;
 
 //routes
 //Greeting
+//http://localhost:3000/greeting
 app.get('/greeting',(req,res)=>{
     res.send('Hello, stranger')
-})
+});
 
+//http://localhost:3000/greeting/Larissa
 app.get('/greeting/:name',(req,res)=>{
     res.send("Oh, what a lovely name! Nice to meet you, "+req.params.name +'.');
-})
+});
 
-//Tip Calculator
+//Tip Calculator 
+//http://localhost:3000/tip/100/20
 app.get('/tip/:total/:tipPercentage',(req,res)=>{
     let tip=parseInt(req.params.total)*(parseInt(req.params.tipPercentage)/100);
     res.send(tip.toString());
+});
+
+//Magic 8 Ball 
+//our data
+const magic8= require('./models/magic8');
+app.get('/magic/:question',(req,res)=>{
+    let rnd=magic8[Math.floor(Math.random()*magic8.length)]
+    res.send(`${req.params.question}? <h1> ${rnd} </h1>`);
+
 })
 
 app.listen(port,()=>{
